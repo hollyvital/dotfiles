516fa42a0e3d9b1223742821c43568edcb31c9e8
hollyvital <holly@vitalbio.com>
2021-09-22 12:54:11 -0400

    Added more notes, update_git_repos is not longer helpful for my setup

--- update_git_repos.sh
+++ update_git_repos.sh
@@ -1,14 +1,2 @@
-#!/bin/sh
-cd krakoa/
-git pull
-printf "Krakoa updated\n=======================\n"
-cd ../vitalone-device
 git pull
 git submodule update --init --recursive
-printf "Vitalone Device and submodules updated\n=======================\n"
-cd ../software-operations
-git pull
-printf "Software-operations updated\n=======================\n"
-cd ../vital-nix
-git pull
-printf "Vital Nix updated\n=======================\n"
